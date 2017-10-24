35608b442ae6281caa3703c0fb8573d11214ffa1
Joyee Cheung <joyeec9h3@gmail.com>
2017-10-25 03:02:39 +0800

    refactor bin and check, implement 48-hour wait

--- bin/metadata.js
+++ bin/metadata.js
@@ -16,25 +16,16 @@ const COMMENTS_QUERY = loadQuery('PRComments');
 const { request, requestAll } = require('../lib/request');
 const { getCollaborators } = require('../lib/collaborators');
 const logger = require('../lib/logger');
-const LinkParser = require('../lib/links');
 const ReviewAnalyzer = require('../lib/reviews');
+const PRChecker = require('../lib/pr_checker');
+const MetadataGenerator = require('../lib/metadata_gen');
 
 // const REFERENCE_RE = /referenced this pull request in/
 
 const OWNER = 'nodejs';
 const REPO = 'node';
 
-const PR_ID = parseInt(process.argv[2]) || 14782;  // example
-
-function getHint(people) {
-  const fromTSC = people.filter((p) => p.reviewer.isTSC());
-  let hint = '';
-  if (fromTSC.length > 0) {
-    const list = `(${fromTSC.map((p) => p.reviewer.login).join(', ')})`;
-    hint = `, ${fromTSC.length} from TSC ${list}`;
-  }
-  return hint;
-}
+const PR_ID = parseInt(process.argv[2]);  // example
 
 async function main(prid, owner, repo) {
   logger.trace(`Getting collaborator contacts from README of ${owner}/${repo}`);
@@ -43,7 +34,6 @@ async function main(prid, owner, repo) {
   logger.trace(`Getting PR from ${owner}/${repo}/pull/${prid}`);
   const prData = await request(PR_QUERY, { prid, owner, repo });
   const pr = prData.repository.pullRequest;
-  const prUrl = pr.url;
 
   const vars = { prid, owner, repo };
   logger.trace(`Getting reviews from ${owner}/${repo}/pull/${prid}`);
@@ -55,9 +45,6 @@ async function main(prid, owner, repo) {
     'repository', 'pullRequest', 'comments'
   ]);
 
-  // TODO: check committers against authors
-  // TODO: check CI runs
-  // TODO: maybe invalidate review after new commits?
   // logger.trace(`Getting commits from ${owner}/${repo}/pull/${prid}`);
   // const commits = await requestAll(COMMITS_QUERY, vars, [
   //   'repository', 'pullRequest', 'commits'
@@ -65,62 +52,16 @@ async function main(prid, owner, repo) {
 
   const analyzer = new ReviewAnalyzer(reviews, comments, collaborators);
   const reviewers = analyzer.getReviewers();
-  if (reviewers.rejected.length > 0) {
-    for (const { reviewer, review } of reviewers.rejected) {
-      logger.warn(`${reviewer.getName()}) rejected in ${review.ref}`);
-    }
-  }
-  if (reviewers.approved.length === 0) {
-    logger.warn('This PR has not been approved yet');
-  } else {
-    for (const { reviewer, review } of reviewers.approved) {
-      if (review.source === ReviewAnalyzer.SOURCES.FROM_COMMENT) {
-        logger.info(`${reviewer.getName()}) approved in via LGTM in comments`);
-      }
-    }
-  }
+  const metadata = new MetadataGenerator(repo, pr, reviewers).getMetadata();
+  logger.info({ raw: metadata }, 'Generated metadata:');
 
-  const reviewedBy = reviewers.approved;
-  const parser = new LinkParser(repo, pr.bodyHTML);
-  const fixes = parser.getFixes();
-  const refs = parser.getRefs();
-
-  const output = {
-    prUrl, reviewedBy, fixes, refs
-  };
-
-  let meta = [
-    '-------------------------------- >8 --------------------------------',
-    `PR-URL: ${output.prUrl}`
-  ];
-  meta = meta.concat(output.reviewedBy.map((r) => {
-    return `Reviewed-By: ${r.reviewer.getContact()}>`;
-  }));
-  meta = meta.concat(output.fixes.map((fix) => `Fixes: ${fix}`));
-  meta = meta.concat(output.refs.map((ref) => `Refs: ${ref}`));
-  meta.push(
-    '-------------------------------- 8< --------------------------------'
-  );
-  logger.info({ raw: meta.join('\n') }, 'Generated metadta:');
-
-  if (reviewers.rejected.length === 0) {
-    logger.info(`Rejections: 0`);
-  } else {
-    const hint = getHint(reviewers.rejected);
-    logger.warn(`Rejections: ${reviewers.rejected.length}${hint}`);
-  }
-  if (reviewers.approved.length === 0) {
-    logger.warn(`Approvals: 0`);
-  } else {
-    const hint = getHint(reviewers.approved);
-    logger.info(`Approvals: ${reviewers.approved.length}${hint}`);
-    const labels = pr.labels.nodes.map((l) => l.name);
-    if (labels.includes('semver-major')) {
-      if (reviewers.approved.filter((p) => p.reviewer.isTSC()) < 2) {
-        logger.warn('semver-major requires at least two TSC approvals');
-      }
-    }
-  }
+  const checker = new PRChecker(pr, reviewers, comments);
+  checker.checkReviewers();
+  checker.checkReviews();
+  checker.checkPRWait();
+  // TODO: check committers against authors
+  // TODO: check CI runs
+  // TODO: maybe invalidate review after new commits?
 }
 
 main(PR_ID, OWNER, REPO).catch((err) => {
