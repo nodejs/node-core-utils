f67d38466bb60f95587d893d2ed02893e6a611e0
Yuta Hiroto <git@about-hiroppy.com>
2017-10-26 10:58:26 -0700

    add Error when PR id is undefined

--- bin/metadata.js
+++ bin/metadata.js
@@ -70,6 +70,8 @@ main(PR_ID, OWNER, REPO).catch((err) => {
 });
 
 function parsePRId(id) {
+  if (id === undefined)
+    throw new Error('PR id is undefined');
   // Fast path: numeric string
   if (`${+id}` === id)
     return +id;
