852ae57f4b2a40f4ce1c43de4ca2334d8603c8d5
Joyee Cheung <joyeec9h3@gmail.com>
2017-10-30 22:05:57 +0800

    inject logger as a dependency

---
+++ test/fixtures/test_logger.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class TestLogger {
+  constructor() {
+    this.logs = {
+      warn: [],
+      info: [],
+      trace: [],
+      error: []
+    };
+  }
+
+  isEmpty() {
+    for (const key of Object.keys(this.logs)) {
+      if (this.logs[key].length !== 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  warn(...args) {
+    this.logs.warn.push(args);
+  }
+
+  info(...args) {
+    this.logs.info.push(args);
+  }
+
+  trace(...args) {
+    this.logs.trace.push(args);
+  }
+
+  error(...args) {
+    this.logs.error.push(args);
+  }
+};
+
+module.exports = TestLogger;
--- test/intergration/collaborators.test.js
+++ test/intergration/collaborators.test.js
@@ -3,16 +3,19 @@
 const {
   getCollaborators, Collaborator
 } = require('../../lib/collaborators');
+const TestLogger = require('../fixtures/test_logger');
+const logger = new TestLogger();
 const assert = require('assert');
 
 describe('collaborators intergration', function() {
   it('getCollaborators', async function() {
     this.timeout(10000);
-    const collaborators = await getCollaborators('nodejs', 'node');
+    const collaborators = await getCollaborators(logger, 'nodejs', 'node');
     assert(collaborators instanceof Map);
     // first of the list
     assert(collaborators.get('addaleax') instanceof Collaborator);
     // last of the list
     assert(collaborators.get('yosuke-furukawa') instanceof Collaborator);
+    assert(logger.isEmpty());
   });
 });
