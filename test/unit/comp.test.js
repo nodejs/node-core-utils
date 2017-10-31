78f2a98cdbb0a19a788f321ee729d801c644dac9
Jon Moss <maclover7@users.noreply.github.com>
2017-10-31 10:22:05 -0400

    test: add simple tests for comparisons (#23)
    * test: add simple tests for comparisons

---
+++ test/unit/comp.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('assert');
+const { ascending, descending } = require('../../lib/comp');
+
+const arr = [
+  '2017-10-30T15:47:52Z',
+  '2017-10-30T15:19:37Z'
+];
+
+describe('Comparison', () => {
+  it('should sort by ascending', () => {
+    assert.deepStrictEqual(
+      ['2017-10-30T15:19:37Z', '2017-10-30T15:47:52Z'],
+      arr.sort(ascending)
+    );
+
+    assert.strictEqual(ascending(0, 1), -1);
+    assert.strictEqual(ascending(1, 0), 1);
+  });
+
+  it('should sort by descending', () => {
+    assert.deepStrictEqual(
+      ['2017-10-30T15:47:52Z', '2017-10-30T15:19:37Z'],
+      arr.sort(descending)
+    );
+
+    assert.strictEqual(descending(0, 1), 1);
+    assert.strictEqual(descending(1, 0), -1);
+  });
+});
