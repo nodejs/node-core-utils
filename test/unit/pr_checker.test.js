6d1ce1d06157ad7e038a4d1f2927a81a79bfc29d
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-01 23:31:00 +0800

    Wrap test cases in suites, test checkAll

--- test/unit/pr_checker.test.js
+++ test/unit/pr_checker.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assert = require('assert');
+const sinon = require('sinon');
 
 const { readJSON, patchPrototype } = require('../fixtures');
 const TestLogger = require('../fixtures/test_logger');
@@ -39,215 +40,265 @@ const firstTimerPR = readJSON('first_timer_pr.json');
 const semverMajorPR = readJSON('semver_major_pr.json');
 
 describe('PRChecker', () => {
-  it('should warn about semver-major PR without enough TSC approvals', () => {
+  describe('checkAll', () => {
     const logger = new TestLogger();
-
-    const expectedLogs = {
-      warn: [
-        ['semver-major requires at least two TSC approvals']
-      ],
-      info: [
-        ['Rejections: 0'],
-        ['Approvals: 3, 1 from TSC (bar)'],
-        ['Bar User(bar)) approved in via LGTM in comments']
-      ],
-      error: [],
-      trace: []
-    };
-
     const checker = new PRChecker(logger,
-      semverMajorPR,
+      firstTimerPR,
       allGreenReviewers,
       commentsWithLGTM,
       approvingReviews,
       simpleCommits,
       collaborators);
 
-    checker.checkReviews();
-    assert.deepStrictEqual(logger.logs, expectedLogs);
-  });
+    let checkReviewsStub;
+    let checkPRWaitStub;
+    let checkCIStub;
+    let authorIsNewStub;
+    let checkAuthorStub;
 
-  it('should warn about PR with rejections & without approvals', () => {
-    const logger = new TestLogger();
-
-    const expectedLogs = {
-      warn: [
-        ['Rejections: 2, 1 from TSC (bar)'],
-        ['Foo User(foo)) rejected in https://github.com/nodejs/node/pull/16438#pullrequestreview-71480624'],
-        ['Bar User(bar)) rejected in https://github.com/nodejs/node/pull/16438#pullrequestreview-71482624'],
-        ['Approvals: 0']
-      ],
-      info: [],
-      error: [],
-      trace: []
-    };
-
-    const checker = new PRChecker(logger,
-      firstTimerPR,
-      rejectedReviewers,
-      [],
-      rejectingReviews,
-      simpleCommits,
-      collaborators);
-
-    checker.checkReviews();
-    assert.deepStrictEqual(logger.logs, expectedLogs);
-  });
-
-  it('should warn about PR younger than 72h on weekends', () => {
-    const logger = new TestLogger();
-
-    const expectedLogs = {
-      warn: [ ['49 hours left to land'] ],
-      info: [ ['This PR was created on Fri Oct 27 2017 (weekend in UTC)'] ],
-      error: [],
-      trace: []
-    };
-
-    const now = new Date('2017-10-28T13:00:41.682Z');
-    const youngPR = Object.assign({}, firstTimerPR, {
-      createdAt: '2017-10-27T14:25:41.682Z'
+    before(() => {
+      checkReviewsStub = sinon.stub(checker, 'checkReviews');
+      checkPRWaitStub = sinon.stub(checker, 'checkPRWait');
+      checkCIStub = sinon.stub(checker, 'checkCI');
+      authorIsNewStub = sinon.stub(checker, 'authorIsNew').returns(true);
+      checkAuthorStub = sinon.stub(checker, 'checkAuthor');
     });
 
-    const checker = new PRChecker(logger,
-      youngPR,
-      allGreenReviewers,
-      commentsWithLGTM,
-      approvingReviews,
-      simpleCommits,
-      collaborators);
-
-    checker.checkPRWait(now);
-    assert.deepStrictEqual(logger.logs, expectedLogs);
-  });
-
-  it('should warn about PR younger than 48h on weekdays', () => {
-    const logger = new TestLogger();
-
-    const expectedLogs = {
-      warn: [ ['22 hours left to land'] ],
-      info: [ ['This PR was created on Tue Oct 31 2017 (weekday in UTC)'] ],
-      error: [],
-      trace: []
-    };
-
-    const now = new Date('2017-11-01T14:25:41.682Z');
-    const youngPR = Object.assign({}, firstTimerPR, {
-      createdAt: '2017-10-31T13:00:41.682Z'
+    after(() => {
+      checkReviewsStub.restore();
+      checkPRWaitStub.restore();
+      checkCIStub.restore();
+      authorIsNewStub.restore();
+      checkAuthorStub.restore();
     });
 
-    const checker = new PRChecker(logger,
-      youngPR,
-      allGreenReviewers,
-      commentsWithLGTM,
-      approvingReviews,
-      simpleCommits,
-      collaborators);
-
-    checker.checkPRWait(now);
-    assert.deepStrictEqual(logger.logs, expectedLogs);
+    it('should run necessary checks', () => {
+      checker.checkAll();
+      assert.strictEqual(checkReviewsStub.calledOnce, true);
+      assert.strictEqual(checkPRWaitStub.calledOnce, true);
+      assert.strictEqual(checkCIStub.calledOnce, true);
+      assert.strictEqual(authorIsNewStub.calledOnce, true);
+      assert.strictEqual(checkAuthorStub.calledOnce, true);
+    });
   });
 
-  it('should warn if no CI runs detected', () => {
-    const logger = new TestLogger();
+  describe('checkReviews', () => {
+    it('should warn about semver-major PR without enough TSC approvals', () => {
+      const logger = new TestLogger();
 
-    const expectedLogs = {
-      warn: [
-        ['No CI runs detected']
-      ],
-      info: [],
-      error: [],
-      trace: []
-    };
+      const expectedLogs = {
+        warn: [
+          ['semver-major requires at least two TSC approvals']
+        ],
+        info: [
+          ['Rejections: 0'],
+          ['Approvals: 3, 1 from TSC (bar)'],
+          ['Bar User(bar)) approved in via LGTM in comments']
+        ],
+        error: [],
+        trace: []
+      };
 
-    const checker = new PRChecker(logger,
-      firstTimerPR,
-      allGreenReviewers,
-      commentsWithLGTM,
-      approvingReviews,
-      simpleCommits,
-      collaborators);
+      const checker = new PRChecker(logger,
+        semverMajorPR,
+        allGreenReviewers,
+        commentsWithLGTM,
+        approvingReviews,
+        simpleCommits,
+        collaborators);
 
-    checker.checkCI();
-    assert.deepStrictEqual(logger.logs, expectedLogs);
+      checker.checkReviews();
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
+
+    it('should warn about PR with rejections & without approvals', () => {
+      const logger = new TestLogger();
+
+      const expectedLogs = {
+        warn: [
+          ['Rejections: 2, 1 from TSC (bar)'],
+          ['Foo User(foo)) rejected in https://github.com/nodejs/node/pull/16438#pullrequestreview-71480624'],
+          ['Bar User(bar)) rejected in https://github.com/nodejs/node/pull/16438#pullrequestreview-71482624'],
+          ['Approvals: 0']
+        ],
+        info: [],
+        error: [],
+        trace: []
+      };
+
+      const checker = new PRChecker(logger,
+        firstTimerPR,
+        rejectedReviewers,
+        [],
+        rejectingReviews,
+        simpleCommits,
+        collaborators);
+
+      checker.checkReviews();
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
   });
 
-  it('should summarize CI runs detected', () => {
-    const logger = new TestLogger();
+  describe('checkPRWait', () => {
+    it('should warn about PR younger than 72h on weekends', () => {
+      const logger = new TestLogger();
 
-    const expectedLogs = {
-      warn: [],
-      info: [
-        [
-          'Last Full CI on 2017-10-27T04:16:36.458Z: ' +
-          'https://ci.nodejs.org/job/node-test-pull-request/10984/'
-        ],
-        [
-          'Last CITGM CI on 2017-10-27T04:16:36.458Z: ' +
-          'https://ci.nodejs.org/view/Node.js-citgm/job/citgm-smoker/1030/'
-        ],
-        [
-          'Last libuv CI on 2017-10-24T04:16:36.458Z: ' +
-          'https://ci.nodejs.org/view/libuv/job/libuv-test-commit/537/'
-        ],
-        [
-          'Last No Intl CI on 2017-10-23T04:16:36.458Z: ' +
-          'https://ci.nodejs.org/job/node-test-commit-linux-nointl/7'
-        ],
-        [
-          'Last V8 CI on 2017-10-22T04:16:36.458Z: ' +
-          'https://ci.nodejs.org/job/node-test-commit-v8-linux/1018/'
-        ],
-        [
-          'Last Benchmark CI on 2017-10-21T04:16:36.458Z: ' +
-          'https://ci.nodejs.org/job/benchmark-node-micro-benchmarks/20/'
-        ],
-        [
-          'Last Linter CI on 2017-10-22T04:16:36.458Z: ' +
-          'https://ci.nodejs.org/job/node-test-linter/13127/'
-        ]
-      ],
-      error: [],
-      trace: []
-    };
+      const expectedLogs = {
+        warn: [['49 hours left to land']],
+        info: [['This PR was created on Fri Oct 27 2017 (weekend in UTC)']],
+        error: [],
+        trace: []
+      };
 
-    const checker = new PRChecker(logger,
-      firstTimerPR,
-      allGreenReviewers,
-      commentsWithCI,
-      approvingReviews,
-      simpleCommits,
-      collaborators);
+      const now = new Date('2017-10-28T13:00:41.682Z');
+      const youngPR = Object.assign({}, firstTimerPR, {
+        createdAt: '2017-10-27T14:25:41.682Z'
+      });
 
-    checker.checkCI();
-    assert.deepStrictEqual(logger.logs, expectedLogs);
+      const checker = new PRChecker(logger,
+        youngPR,
+        allGreenReviewers,
+        commentsWithLGTM,
+        approvingReviews,
+        simpleCommits,
+        collaborators);
+
+      checker.checkPRWait(now);
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
+
+    it('should warn about PR younger than 48h on weekdays', () => {
+      const logger = new TestLogger();
+
+      const expectedLogs = {
+        warn: [['22 hours left to land']],
+        info: [['This PR was created on Tue Oct 31 2017 (weekday in UTC)']],
+        error: [],
+        trace: []
+      };
+
+      const now = new Date('2017-11-01T14:25:41.682Z');
+      const youngPR = Object.assign({}, firstTimerPR, {
+        createdAt: '2017-10-31T13:00:41.682Z'
+      });
+
+      const checker = new PRChecker(logger,
+        youngPR,
+        allGreenReviewers,
+        commentsWithLGTM,
+        approvingReviews,
+        simpleCommits,
+        collaborators);
+
+      checker.checkPRWait(now);
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
   });
 
-  it('should check odd commits for first timers', () => {
-    const logger = new TestLogger();
+  describe('checkCI', () => {
+    it('should warn if no CI runs detected', () => {
+      const logger = new TestLogger();
 
-    const expectedLogs = {
-      warn: [
-        ['PR is opened by @pr_author'],
-        [ 'Author test@example.com of commit e3ad7c7 ' +
-          'does not match committer or PR author' ],
-        [ 'Author test@example.com of commit da39a3e ' +
-          'does not match committer or PR author']
-      ],
-      info: [],
-      error: [],
-      trace: []
-    };
+      const expectedLogs = {
+        warn: [
+          ['No CI runs detected']
+        ],
+        info: [],
+        error: [],
+        trace: []
+      };
 
-    const checker = new PRChecker(logger, firstTimerPR,
-      allGreenReviewers,
-      commentsWithLGTM,
-      approvingReviews,
-      oddCommits,
-      collaborators);
+      const checker = new PRChecker(logger,
+        firstTimerPR,
+        allGreenReviewers,
+        commentsWithLGTM,
+        approvingReviews,
+        simpleCommits,
+        collaborators);
 
-    assert(checker.authorIsNew());
-    checker.checkAuthor();
-    assert.deepStrictEqual(logger.logs, expectedLogs);
+      checker.checkCI();
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
+
+    it('should summarize CI runs detected', () => {
+      const logger = new TestLogger();
+
+      const expectedLogs = {
+        warn: [],
+        info: [
+          [
+            'Last Full CI on 2017-10-27T04:16:36.458Z: ' +
+            'https://ci.nodejs.org/job/node-test-pull-request/10984/'
+          ],
+          [
+            'Last CITGM CI on 2017-10-27T04:16:36.458Z: ' +
+            'https://ci.nodejs.org/view/Node.js-citgm/job/citgm-smoker/1030/'
+          ],
+          [
+            'Last libuv CI on 2017-10-24T04:16:36.458Z: ' +
+            'https://ci.nodejs.org/view/libuv/job/libuv-test-commit/537/'
+          ],
+          [
+            'Last No Intl CI on 2017-10-23T04:16:36.458Z: ' +
+            'https://ci.nodejs.org/job/node-test-commit-linux-nointl/7'
+          ],
+          [
+            'Last V8 CI on 2017-10-22T04:16:36.458Z: ' +
+            'https://ci.nodejs.org/job/node-test-commit-v8-linux/1018/'
+          ],
+          [
+            'Last Benchmark CI on 2017-10-21T04:16:36.458Z: ' +
+            'https://ci.nodejs.org/job/benchmark-node-micro-benchmarks/20/'
+          ],
+          [
+            'Last Linter CI on 2017-10-22T04:16:36.458Z: ' +
+            'https://ci.nodejs.org/job/node-test-linter/13127/'
+          ]
+        ],
+        error: [],
+        trace: []
+      };
+
+      const checker = new PRChecker(logger,
+        firstTimerPR,
+        allGreenReviewers,
+        commentsWithCI,
+        approvingReviews,
+        simpleCommits,
+        collaborators);
+
+      checker.checkCI();
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
+  });
+
+  describe('authorIsNew/checkAuthor', () => {
+    it('should check odd commits for first timers', () => {
+      const logger = new TestLogger();
+
+      const expectedLogs = {
+        warn: [
+          ['PR is opened by @pr_author'],
+          ['Author test@example.com of commit e3ad7c7 ' +
+            'does not match committer or PR author'],
+          ['Author test@example.com of commit da39a3e ' +
+            'does not match committer or PR author']
+        ],
+        info: [],
+        error: [],
+        trace: []
+      };
+
+      const checker = new PRChecker(logger, firstTimerPR,
+        allGreenReviewers,
+        commentsWithLGTM,
+        approvingReviews,
+        oddCommits,
+        collaborators);
+
+      assert(checker.authorIsNew());
+      checker.checkAuthor();
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
   });
 });
