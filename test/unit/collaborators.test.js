c4a51705c9c436371fd16e9a3c1aa8f0c4c5647b
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-02 19:40:42 +0800

    refactor the request class

--- test/unit/collaborators.test.js
+++ test/unit/collaborators.test.js
@@ -3,15 +3,17 @@
 const {
   getCollaborators, Collaborator
 } = require('../../lib/collaborators');
+const { readFile } = require('../fixtures');
 const TestLogger = require('../fixtures/test_logger');
 const logger = new TestLogger();
 const assert = require('assert');
-const requestPromise = require('request-promise-native');
+const readme = readFile('README.md');
 
-describe('collaborators intergration', function() {
+describe('collaborators', function() {
+  // TODO: make a fake README.md, or generate one with existing collaborators
+  // Test type=TSC and stuff
   it('getCollaborators', async function() {
-    this.timeout(10000);
-    const collaborators = await getCollaborators(requestPromise, logger, 'nodejs', 'node');
+    const collaborators = await getCollaborators(readme, logger, 'nodejs', 'node');
     assert(collaborators instanceof Map);
     // first of the list
     assert(collaborators.get('addaleax') instanceof Collaborator);
