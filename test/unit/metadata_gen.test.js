b413e0b4590a9b00af680ba2c09b485b06125ff2
Anatoli Papirovski <apapirovski@mac.com>
2017-11-04 13:07:24 -0400

    fix the parsing of fixes

--- test/unit/metadata_gen.test.js
+++ test/unit/metadata_gen.test.js
@@ -9,13 +9,14 @@ const {
 const { EOL } = require('os');
 const assert = require('assert');
 const data = {
+  owner: 'nodejs',
   repo: 'node',
   pr: fixAndRefPR,
   reviewers: allGreenReviewers
 };
 
 const expected = `PR-URL: https://github.com/nodejs/node/pull/16438
-Fixes: https://github.com/node/issues/16437
+Fixes: https://github.com/nodejs/node/issues/16437
 Refs: https://github.com/nodejs/node/pull/15148
 Reviewed-By: Foo User <foo@example.com>
 Reviewed-By: Baz User <baz@example.com>
