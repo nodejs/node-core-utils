341314beb3c782834154670d5eca57b8018494bc
Alejandro López <a.lopez.sanchez@outlook.es>
2017-11-05 23:11:27 +0100

    test collaborators: apply max line length and correct ghost user references

--- test/unit/collaborators.test.js
+++ test/unit/collaborators.test.js
@@ -6,7 +6,15 @@ const {
   isCollaborator,
   getCollaborators
 } = require('../../lib/collaborators');
-const { readme, readmeNoTsc, readmeNoTscE, readmeNoCollaborators, readmeNoCollaboratorE, readmeUnordered, collaborators } = require('../fixtures/data');
+const {
+  readme,
+  readmeNoTsc,
+  readmeNoTscE,
+  readmeNoCollaborators,
+  readmeNoCollaboratorE,
+  readmeUnordered,
+  collaborators
+} = require('../fixtures/data');
 const TestLogger = require('../fixtures/test_logger');
 const assertThrowsAsync = require('../fixtures/assert_throws_async');
 
@@ -24,8 +32,8 @@ describe('collaborators', function() {
 
   describe('Collaborator', () => {
     describe('isActor', () => {
-      it('should return false if no actor is specified', () => {
-        assert.strictEqual(collaborator.isActor(), false);
+      it('should return false if actor is a ghost', () => {
+        assert.strictEqual(collaborator.isActor(null), false);
       });
 
       it('should return false if the actor has no login', () => {
@@ -37,9 +45,11 @@ describe('collaborators', function() {
         assert.strictEqual(collaborator.isActor(collaboratorNoLogin), false);
       });
 
-      it('should return true if the the collaborator and actor login  are the same', () => {
-        assert.strictEqual(collaborator.isActor(collaborator), true);
-      });
+      it(
+        'should return true if the the collaborator and actor login are equal',
+        () => {
+          assert.strictEqual(collaborator.isActor(collaborator), true);
+        });
     });
 
     describe('isTSC', () => {
@@ -58,7 +68,9 @@ describe('collaborators', function() {
     describe('getName', () => {
       it('should return the name and login', () => {
         collaborators.forEach(collaborator => {
-          assert.strictEqual(collaborator.getName(), `${collaborator.name}(${collaborator.login})`);
+          assert.strictEqual(
+            collaborator.getName(),
+            `${collaborator.name}(${collaborator.login})`);
         });
       });
     });
@@ -66,7 +78,9 @@ describe('collaborators', function() {
     describe('getContact', () => {
       it('should return the name and the email', () => {
         collaborators.forEach(collaborator => {
-          assert.strictEqual(collaborator.getContact(), `${collaborator.name} <${collaborator.email}>`);
+          assert.strictEqual(
+            collaborator.getContact(),
+            `${collaborator.name} <${collaborator.email}>`);
         });
       });
     });
@@ -78,43 +92,82 @@ describe('collaborators', function() {
       assert.deepStrictEqual(parsed, collaborators);
     });
 
-    it('should throw error if there is no TSC section in the README', async() => {
-      await assertThrowsAsync(async() => getCollaborators(readmeNoTsc, logger, 'nodejs', 'node'), /Error: Couldn't find ### TSC \(Technical Steering Committee\) in the README/);
-    });
+    it(
+      'should throw error if there is no TSC section in the README',
+      async() => {
+        await assertThrowsAsync(
+          async() => getCollaborators(readmeNoTsc, logger, 'nodejs', 'node'),
+          Error);
+      });
 
-    it('should throw error if there is no TSC Emeriti section in the README', async() => {
-      await assertThrowsAsync(async() => getCollaborators(readmeNoTscE, logger, 'nodejs', 'node'), /Error: Couldn't find ### TSC Emeriti in the README/);
-    });
+    it(
+      'should throw error if there is no TSC Emeriti section in the README',
+      async() => {
+        await assertThrowsAsync(
+          async() => getCollaborators(readmeNoTscE, logger, 'nodejs', 'node'),
+          /Error: Couldn't find ### TSC Emeriti in the README/);
+      });
 
-    it('should throw error if there is no Collaborators section in the README', async() => {
-      await assertThrowsAsync(async() => getCollaborators(readmeNoCollaborators, logger, 'nodejs', 'node'), /Error: Couldn't find ### Collaborators in the README/);
-    });
+    it('should throw error if there is no Collaborators section in the README',
+      async() => {
+        await assertThrowsAsync(
+          async() => getCollaborators(
+            readmeNoCollaborators, logger, 'nodejs', 'node'),
+          /Error: Couldn't find ### Collaborators in the README/);
+      });
 
-    it('should throw error if there is no Collaborator Emeriti section in the README', async() => {
-      await assertThrowsAsync(async() => getCollaborators(readmeNoCollaboratorE, logger, 'nodejs', 'node'), /Error: Couldn't find ### Collaborator Emeriti in the README/);
-    });
+    it(
+      'should throw error if there is no Collaborator' +
+      'Emeriti section in the README',
+      async() => {
+        await assertThrowsAsync(
+          async() => getCollaborators(
+            readmeNoCollaboratorE, logger, 'nodejs', 'node'),
+          /Error: Couldn't find ### Collaborator Emeriti in the README/);
+      });
 
-    it('should WARN if the TSC and Collaborators section are not ordered in the README', async() => {
-      await assertThrowsAsync(async() => getCollaborators(readmeUnordered, logger, 'nodejs', 'node'), /Error/);
-      assert.strictEqual(logger.logs.warn[0][0], 'Order of different types of contacts needs update. Analysis could get wrong.');
-    });
+    it(
+      'should WARN if the TSC and Collaborators' +
+      'section are not ordered in the README',
+      async() => {
+        await assertThrowsAsync(
+          async() => getCollaborators(
+            readmeUnordered, logger, 'nodejs', 'node'),
+          /Error/);
+        assert.strictEqual(logger.logs.warn[0][0],
+          'Order of different types of contacts needs update. ' +
+          'Analysis could get wrong.');
+      });
 
     it('should throw error if there are no collaborators', async() => {
-      await assertThrowsAsync(async() => getCollaborators(readmeUnordered, logger, 'nodejs', 'node'), /Error: Could not find any collaborators/);
+      await assertThrowsAsync(
+        async() => getCollaborators(readmeUnordered, logger, 'nodejs', 'node'),
+        /Error: Could not find any collaborators/);
     });
   });
 
   describe('isCollaborator', () => {
-    it('should return undefined if the user has no login (ghost)', () => {
-      assert.strictEqual(isCollaborator(collaborators, { name: 'Ghost' }), undefined);
+    it('should return null if the user is a ghost', () => {
+      assert.strictEqual(
+        isCollaborator(collaborators, null), null);
     });
 
-    it('should return undefined if the user is valid but is not a collaborator', () => {
-      assert.strictEqual(isCollaborator(collaborators, { name: 'User', login: 'user', email: 'user@example.com' }), undefined);
-    });
+    it(
+      'should return undefined if the user is valid ' +
+      'but is not a collaborator', () => {
+        assert.strictEqual(
+          isCollaborator(
+            collaborators,
+            { name: 'User', login: 'user', email: 'user@example.com' }),
+          undefined);
+      });
 
-    it('should return the collaborator info if the user is in the collaborators list', () => {
-      assert.strictEqual(isCollaborator(collaborators, collaborator), collaborator);
-    });
+    it(
+      'should return the collaborator info if' +
+      ' the user is in the collaborators list',
+      () => {
+        assert.strictEqual(
+          isCollaborator(collaborators, collaborator), collaborator);
+      });
   });
 });
