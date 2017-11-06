536f379c7fee832c37d9c892e76a9c524fc0288b
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-06 19:38:34 +0800

    bin: make getMetadata a function

--- bin/get_metadata.js
+++ bin/get_metadata.js
@@ -1,45 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 
-const Request = require('../lib/request');
-const auth = require('../lib/auth');
-
-const loggerFactory = require('../lib/logger');
-const PRData = require('../lib/pr_data');
-const PRChecker = require('../lib/pr_checker');
-const MetadataGenerator = require('../lib/metadata_gen');
 const argv = require('../lib/args')();
+const getMetadata = require('./metadata');
+const loggerFactory = require('../lib/logger');
 
-// const REFERENCE_RE = /referenced this pull request in/
 const OWNER = argv.owner;
 const REPO = argv.repo;
 const PR_ID = argv.id;
 
-async function main(prid, owner, repo, logger) {
-  const credentials = await auth();
-  const request = new Request(credentials);
-
-  const data = new PRData(prid, owner, repo, logger, request);
-  await data.getAll();
-
-  data.logIntro();
-
-  const metadata = new MetadataGenerator(data).getMetadata();
-  const [SCISSOR_LEFT, SCISSOR_RIGHT] = MetadataGenerator.SCISSORS;
-  logger.info({
-    raw: `${SCISSOR_LEFT}${metadata}${SCISSOR_RIGHT}`
-  }, 'Generated metadata:');
-
-  if (!process.stdout.isTTY) {
-    process.stdout.write(metadata);
-  }
-  const checker = new PRChecker(logger, data);
-  checker.checkAll();
-}
-
 const logStream = process.stdout.isTTY ? process.stdout : process.stderr;
 const logger = loggerFactory(logStream);
-main(PR_ID, OWNER, REPO, logger).catch((err) => {
+
+getMetadata(PR_ID, OWNER, REPO, logger).catch((err) => {
   logger.error(err);
   process.exit(-1);
 });
