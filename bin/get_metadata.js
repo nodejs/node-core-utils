ccaed607a76f1f16dbaa8c0562dac6fbee559e3c
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-06 19:44:29 +0800

    args: pass the argv as an object

--- bin/get_metadata.js
+++ bin/get_metadata.js
@@ -5,14 +5,10 @@ const argv = require('../lib/args')();
 const getMetadata = require('./metadata');
 const loggerFactory = require('../lib/logger');
 
-const OWNER = argv.owner;
-const REPO = argv.repo;
-const PR_ID = argv.id;
-
 const logStream = process.stdout.isTTY ? process.stdout : process.stderr;
 const logger = loggerFactory(logStream);
 
-getMetadata(PR_ID, OWNER, REPO, logger).catch((err) => {
+getMetadata(argv, logger).catch((err) => {
   logger.error(err);
   process.exit(-1);
 });
