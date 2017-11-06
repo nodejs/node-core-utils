61c2d88afc5180f23de671c75c07c0916755ee55
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-06 20:06:11 +0800

    bin: remove positional arguments

--- test/unit/args.test.js
+++ test/unit/args.test.js
@@ -15,31 +15,8 @@ describe('args', async function() {
       'should return object with default owner and repo ' +
       'if called with numeric argument only',
       async function() {
-        let actual = parseArgs('16637');
-        await assert.deepStrictEqual(actual, expected);
-      });
-
-    it(
-      'should return object with default owner and given repo ' +
-      'if called with 2 arguments',
-      async function() {
-        let actual = parseArgs('16637 nodejs.org');
-        let modExpected = Object.assign({}, expected);
-        modExpected.repo = 'nodejs.org';
-        await assert.deepStrictEqual(actual, modExpected);
-      });
-
-    it(
-      'should return object with given owner and given repo ' +
-      'if called with 3 arguments',
-      async function() {
-        let expect = {
-          owner: 'joyeecheung',
-          repo: 'node-core-utils',
-          prid: 41
-        };
-        let actual = parseArgs('41 joyeecheung node-core-utils');
-        await assert.deepStrictEqual(actual, expect);
+        const actual = parseArgs('16637');
+        assert.deepStrictEqual(actual, expected);
       });
 
     it(
@@ -47,44 +24,42 @@ describe('args', async function() {
       'if called with flagged arguments',
       async function() {
         let actual = parseArgs('16637 -o nodejs -r node');
-        await assert.deepStrictEqual(actual, expected);
+        assert.deepStrictEqual(actual, expected);
         actual = parseArgs('16637 -r node -o nodejs');
-        await assert.deepStrictEqual(actual, expected);
+        assert.deepStrictEqual(actual, expected);
         actual = parseArgs('16637 --owner nodejs --repo node');
-        await assert.deepStrictEqual(actual, expected);
+        assert.deepStrictEqual(actual, expected);
         actual = parseArgs('16637 --repo node --owner nodejs');
-        await assert.deepStrictEqual(actual, expected);
+        assert.deepStrictEqual(actual, expected);
       });
   });
 
   describe('Pull URL', async function() {
     it('should return object with parsed arguments when called with url',
       async function() {
-        let actual = parseArgs('https://github.com/nodejs/node/pull/16637');
-        await assert.deepStrictEqual(actual, expected);
+        const actual = parseArgs('https://github.com/nodejs/node/pull/16637');
+        assert.deepStrictEqual(actual, expected);
       });
   });
 
   describe('Errors', async function() {
     it('should exit and log error when called without arguments',
       async function() {
-        await assert.throws(parseArgs);
+        assert.throws(parseArgs);
       });
 
     it('should throw when called with a non-url string',
       async function() {
-        let result = () => {
+        const result = () => {
           return parseArgs('dummy');
         };
-        await assert.throws(result);
+        assert.throws(result);
       });
 
-    it('should throw if called with url and other argument',
+    it('should ignore other arguments if called with url',
       async function() {
-        let actual = () => {
-          return parseArgs('https://github.com/nodejs/node/pull/16637 nodejs.org');
-        };
-        await assert.throws(actual);
+        const actual = parseArgs('https://github.com/nodejs/node/pull/16637 -o nodejs.org');
+        assert.deepStrictEqual(actual, expected);
       });
   });
 });
