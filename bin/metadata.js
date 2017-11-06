f6796354cd185d40c00466193009a3a3ce46b679
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-06 19:38:34 +0800

    bin: make getMetadata a function

---
+++ bin/metadata.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+'use strict';
+
+const Request = require('../lib/request');
+const auth = require('../lib/auth');
+const PRData = require('../lib/pr_data');
+const PRChecker = require('../lib/pr_checker');
+const MetadataGenerator = require('../lib/metadata_gen');
+
+module.exports = async function getMetadata(prid, owner, repo, logger) {
+  const credentials = await auth();
+  const request = new Request(credentials);
+
+  const data = new PRData(prid, owner, repo, logger, request);
+  await data.getAll();
+
+  data.logIntro();
+
+  const metadata = new MetadataGenerator(data).getMetadata();
+  const [SCISSOR_LEFT, SCISSOR_RIGHT] = MetadataGenerator.SCISSORS;
+  logger.info({
+    raw: `${SCISSOR_LEFT}${metadata}${SCISSOR_RIGHT}`
+  }, 'Generated metadata:');
+
+  if (!process.stdout.isTTY) {
+    process.stdout.write(metadata);
+  }
+  const checker = new PRChecker(logger, data);
+  const status = checker.checkAll();
+  return {
+    status,
+    request,
+    data,
+    metadata,
+    checker
+  };
+};
