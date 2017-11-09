491e2b24ee9dd215fcba7186a36282524390f2ed
Benjamin Zaslavsky <Tiriel@users.noreply.github.com>
2017-11-09 14:40:50 +0100

    Skip wait check when label 'code-and-learn' is found, testing (#92)
    * Skip wait check when label 'code-and-learn' is found, testing
    
    * Simplified condition, removing logged text
    
    * Added 'doc'-only label handling

--- test/unit/pr_checker.test.js
+++ test/unit/pr_checker.test.js
@@ -199,6 +199,42 @@ describe('PRChecker', () => {
       assert(!status);
       assert.deepStrictEqual(logger.logs, expectedLogs);
     });
+
+    it('should skip wait check for Code & Learn PR', () => {
+      const logger = new TestLogger();
+
+      const expectedLogs = {
+        warn: [],
+        info: [],
+        error: [],
+        trace: []
+      };
+
+      const now = new Date();
+      const youngPR = Object.assign({}, firstTimerPR, {
+        createdAt: '2017-10-27T14:25:41.682Z',
+        labels: {
+          nodes: [
+            {
+              name: 'code-and-learn'
+            }
+          ]
+        }
+      });
+
+      const checker = new PRChecker(logger, {
+        pr: youngPR,
+        reviewers: allGreenReviewers,
+        comments: commentsWithLGTM,
+        reviews: approvingReviews,
+        commits: simpleCommits,
+        collaborators
+      });
+
+      const status = checker.checkPRWait(now);
+      assert(status);
+      assert.deepStrictEqual(logger.logs, expectedLogs);
+    });
   });
 
   describe('checkCI', () => {
