379925b5cbde8defcd03fd0a02bbfa92918af004
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-05 20:17:13 +0530

    test: add tests for cross-repository/cross-owner fixes
    Fixes: https://github.com/nodejs/node-core-utils/issues/75

--- test/unit/metadata_gen.test.js
+++ test/unit/metadata_gen.test.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const MetadataGenerator = require('../../lib/metadata_gen');
-const {
-  fixAndRefPR,
-  allGreenReviewers
-} = require('../fixtures/data');
+const { fixAndRefPR, allGreenReviewers } = require('../fixtures/data');
 
 const assert = require('assert');
 const data = {
@@ -27,5 +24,24 @@ describe('MetadataGenerator', () => {
   it('should generate metadata properly', () => {
     const results = new MetadataGenerator(data).getMetadata();
     assert.strictEqual(expected, results);
+  });
+
+  it('should handle cross-owner and cross-repo fixes properly', () => {
+    const { bodyHTML } = fixAndRefPR;
+    const newBodyHTML = bodyHTML.replace('#16437', '#123');
+    const newPR = Object.assign({}, fixAndRefPR, { bodyHTML: newBodyHTML });
+    const newData = Object.assign({}, data, {
+      pr: newPR,
+      owner: 'joyeecheung',
+      repo: 'node-core-utils'
+    });
+
+    const newExpected = expected.replace(
+      'https://github.com/nodejs/node/issues/16437',
+      'https://github.com/joyeecheung/node-core-utils/issues/123'
+    );
+
+    const results = new MetadataGenerator(newData).getMetadata();
+    assert.strictEqual(newExpected, results);
   });
 });
