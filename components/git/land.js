6308317309e5c2f68d9cbf615ae89a472864c8ba
Joyee Cheung <joyeec9h3@gmail.com>
2018-03-06 22:08:46 +0800

    git-node: don't check for wrong branch in --amend or --continue

--- components/git/land.js
+++ components/git/land.js
@@ -103,7 +103,10 @@ module.exports = {
 
 async function main(state, argv, cli, req, dir) {
   let session = new LandingSession(cli, req, dir);
-  if (session.warnForMissing() || session.warnForWrongBranch()) {
+  if (session.warnForMissing()) {
+    return;
+  }
+  if (state !== AMEND && state !== CONTINUE && session.warnForWrongBranch()) {
     return;
   }
 
