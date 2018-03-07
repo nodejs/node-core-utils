2c350ddf55d997b2e812763f88d96a89329f665a
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-07 17:45:34 +0530

    test: add tests for cross-repository/cross-owner fixes (#197)
    Fixes: https://github.com/nodejs/node-core-utils/issues/75

--- test/unit/metadata_gen.test.js
+++ test/unit/metadata_gen.test.js
@@ -3,6 +3,7 @@
 const MetadataGenerator = require('../../lib/metadata_gen');
 const {
   fixAndRefPR,
+  fixCrossPR,
   allGreenReviewers
 } = require('../fixtures/data');
 
@@ -13,10 +14,18 @@ const data = {
   pr: fixAndRefPR,
   reviewers: allGreenReviewers
 };
+const crossData = Object.assign({}, data, { pr: fixCrossPR });
 
 const expected = `PR-URL: https://github.com/nodejs/node/pull/16438
 Fixes: https://github.com/nodejs/node/issues/16437
 Refs: https://github.com/nodejs/node/pull/15148
+Reviewed-By: Foo User <foo@example.com>
+Reviewed-By: Quux User <quux@example.com>
+Reviewed-By: Baz User <baz@example.com>
+Reviewed-By: Bar User <bar@example.com>
+`;
+const crossExpected = `PR-URL: https://github.com/nodejs/node/pull/16438
+Fixes: https://github.com/joyeecheung/node-core-utils/issues/123
 Reviewed-By: Foo User <foo@example.com>
 Reviewed-By: Quux User <quux@example.com>
 Reviewed-By: Baz User <baz@example.com>
@@ -27,5 +36,11 @@ describe('MetadataGenerator', () => {
   it('should generate metadata properly', () => {
     const results = new MetadataGenerator(data).getMetadata();
     assert.strictEqual(expected, results);
+  });
+
+  it('should handle cross-owner and cross-repo fixes properly', () => {
+    const results = new MetadataGenerator(crossData).getMetadata();
+    console.log(results);
+    assert.strictEqual(crossExpected, results);
   });
 });
