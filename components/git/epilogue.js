598a3a23bb7630c19f7eb21ac356361725ecdbcd
Ashok <coderatlabs@gmail.com>
2018-03-23 20:41:01 +0530

    allow git-node land to work with full PR url (#213) (#219)
    * allow git-node land to work with full PR url (#213)
    * replace is-url module with regex validation
    * land $URL: change help text

--- components/git/epilogue.js
+++ components/git/epilogue.js
@@ -11,6 +11,7 @@ $ ncu-config set branch master   # Assuming you are landing commits on master
 $ git checkout master
 $ git node land --abort          # Abort a landing session, just in case
 $ git node land $PRID            # Start a new landing session
+$ git node land $URL             # Start a new landing session using the PR URL
 
 $ git rebase -i upstream/master  # Put "edit" on every commit that's gonna stay
 
