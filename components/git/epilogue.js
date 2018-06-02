77565752117fd7007861498a3a3ab55fc163a28e
Rich Trott <rtrott@gmail.com>
2018-06-02 20:31:22 +0200

    remove outdated instructions

--- components/git/epilogue.js
+++ components/git/epilogue.js
@@ -13,10 +13,7 @@ $ git node land --abort          # Abort a landing session, just in case
 $ git node land $PRID            # Start a new landing session
 $ git node land $URL             # Start a new landing session using the PR URL
 
-$ git rebase -i upstream/master  # Put "edit" on every commit that's gonna stay
-
-$ git node land --amend          # Regenerate commit messages in HEAD
-$ git rebase --continue          # Repeat until the rebase is done
+# Follow instructions provided.
 
 $ git node land --final          # Verify all the commit messages
 ==============================================================================
