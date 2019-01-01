db047ffdb3dba5bf1d7ca2c70d8378dbca08fe36
Joyee Cheung <joyeec9h3@gmail.com>
2019-01-02 03:31:19 +0800

    lib: implement `git node sync`

---
+++ components/git/sync.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const CLI = require('../../lib/cli');
+const { runPromise } = require('../../lib/run');
+const SyncSession = require('../../lib/sync_session');
+
+function builder(yargs) {
+  return yargs
+    .wrap(90);
+}
+
+async function main() {
+  const logStream = process.stdout.isTTY ? process.stdout : process.stderr;
+  const cli = new CLI(logStream);
+  const dir = process.cwd();
+  const session = new SyncSession(cli, dir);
+  await session.sync();
+}
+
+function handler(argv) {
+  return runPromise(main());
+}
+
+module.exports = {
+  command: 'sync',
+  describe: 'Sync the branch specified by ncu-config',
+  builder,
+  handler
+};
