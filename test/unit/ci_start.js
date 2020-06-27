9d2013d782cdaefb2d66338e57c2f50ef69294dd
Matheus Marchini <mmarchini@netflix.com>
2020-06-27 00:36:57 -0700

    ncu-ci: command to start CI for PRs

---
+++ test/unit/ci_start.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const assert = require('assert');
+
+const sinon = require('sinon');
+const FormData = require('form-data');
+
+const {
+  RunPRJob,
+  CI_CRUMB_URL,
+  CI_PR_URL
+} = require('../../lib/ci/run_ci');
+const TestCLI = require('../fixtures/test_cli');
+
+describe('Jenkins', () => {
+  it('should fail if starting node-pull-request fails', async() => {
+    const cli = new TestCLI();
+    const crumb = 'asdf1234';
+    const request = {
+      text: sinon.stub().throws(),
+      json: sinon.stub().withArgs(CI_CRUMB_URL)
+        .returns(Promise.resolve({ crumb }))
+    };
+    const owner = 'nodejs';
+    const repo = 'node-auto-test';
+    const prid = 123456;
+
+    const jobRunner = new RunPRJob(cli, request, owner, repo, prid);
+    assert.strictEqual(await jobRunner.start(), false);
+  });
+
+  it('should return false if crumb fails', async() => {
+    const cli = new TestCLI();
+    const request = {
+      json: sinon.stub().throws()
+    };
+    const owner = 'nodejs';
+    const repo = 'node-auto-test';
+    const prid = 123456;
+
+    const jobRunner = new RunPRJob(cli, request, owner, repo, prid);
+    assert.strictEqual(await jobRunner.start(), false);
+  });
+
+  it('should start node-pull-request', async() => {
+    const cli = new TestCLI();
+    const crumb = 'asdf1234';
+    const owner = 'nodejs';
+    const repo = 'node-auto-test';
+    const prid = 123456;
+
+    sinon.stub(FormData.prototype, 'append').callsFake(function(key, value) {
+      assert.strictEqual(key, 'json');
+      const { parameter } = JSON.parse(value);
+      const expectedParameters = {
+        CERTIFY_SAFE: 'on',
+        TARGET_GITHUB_ORG: owner,
+        TARGET_REPO_NAME: repo,
+        PR_ID: prid,
+        REBASE_ONTO: '<pr base branch>',
+        DESCRIPTION_SETTER_DESCRIPTION: ''
+      };
+      for (const { name, value } of parameter) {
+        assert.strictEqual(value, expectedParameters[name]);
+        delete expectedParameters[name];
+      }
+      assert.strictEqual(Object.keys(expectedParameters).length, 0);
+
+      this._validated = true;
+
+      return FormData.prototype.append.wrappedMethod.bind(this)(key, value);
+    });
+
+    const request = {
+      text: sinon.stub()
+        .callsFake((url, { method, headers, body }) => {
+          assert.strictEqual(url, CI_PR_URL);
+          assert.strictEqual(method, 'POST');
+          assert.deepStrictEqual(headers, { 'Jenkins-Crumb': crumb });
+          assert.ok(body._validated);
+        }),
+      json: sinon.stub().withArgs(CI_CRUMB_URL)
+        .returns(Promise.resolve({ crumb }))
+    };
+    const jobRunner = new RunPRJob(cli, request, owner, repo, prid);
+    assert.ok(await jobRunner.start());
+  });
+});
