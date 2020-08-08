4b32ebc99cbf9b750252cda81ca818b6e3754bd4
Mary Marchini <mmarchini@netflix.com>
2020-08-08 16:53:29 -0700

    chore(land): remove lite-ci checks
    Lite CI is not used anymore, but some PRs got Lite-CI runs after some
    changes to the bot. node-core-utils Lite-CI parsing is not working
    though, therefore ncu will fail to land those PRs. Removing Lite-CI
    checks fix the issue and also reduces complexity.
    
    Fix: https://github.com/nodejs/node-core-utils/issues/466

--- test/unit/ci_type_parser.test.js
+++ test/unit/ci_type_parser.test.js
@@ -44,35 +44,11 @@ const expected = new Map([
     link: 'https://ci.nodejs.org/job/node-test-linter/13127/',
     date: '2017-10-27T04:16:36.458Z',
     jobid: 13127
-  }],
-  ['LITE_COMMIT', {
-    link: 'https://ci.nodejs.org/job/node-test-commit-lite/246/',
-    date: '2017-10-28T04:16:36.458Z',
-    jobid: 246
-  }],
-  ['LITE_PR_PIPELINE', {
-    link: 'https://ci.nodejs.org/job/node-test-pull-request-lite-pipeline/7213/pipeline/',
-    date: '2017-10-29T04:16:36.458Z',
-    jobid: 7213
   }]
 ]);
 describe('JobParser', () => {
   it('should parse CI results', () => {
     const results = new JobParser(commentsWithCI).parse();
     assert.deepStrictEqual([...results.entries()], [...expected.entries()]);
-  });
-
-  it('should parse pipeline links', () => {
-    const data = [{
-      publishedAt: '2017-10-29T04:16:36.458Z',
-      bodyText: '@contributer build started: https://ci.nodejs.org/blue/organizations/jenkins/node-test-pull-request-lite-pipeline/detail/node-test-pull-request-lite-pipeline/3009/pipeline/'
-    }];
-    const results = new JobParser(data).parse();
-    assert.deepStrictEqual([...results.entries()], [
-      ['LITE_PR_PIPELINE', {
-        link: 'https://ci.nodejs.org/blue/organizations/jenkins/node-test-pull-request-lite-pipeline/detail/node-test-pull-request-lite-pipeline/3009/pipeline/',
-        date: '2017-10-29T04:16:36.458Z',
-        jobid: 3009
-      }]]);
   });
 });
