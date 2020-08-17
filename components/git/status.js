8ef4a6224fc58f3ed3fe23e69eb7b8be2a2cc961
Shelley Vohr <shelley.vohr@gmail.com>
2020-08-17 11:32:47 -0700

    feat(git-node): add git-node status

---
+++ components/git/status.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+const { readJson } = require('./../../lib/file');
+const { getNcuDir } = require('./../../lib/config');
+
+const CLI = require('../../lib/cli');
+
+const cli = new CLI();
+
+function handler() {
+  const ncuDir = getNcuDir(process.cwd());
+  const landPath = path.join(ncuDir, 'land');
+
+  if (fs.existsSync(landPath)) {
+    const { state, prid, config } = readJson(landPath);
+    const { username, branch, upstream } = config;
+
+    cli.ok('Landing session in progress');
+    cli.separator();
+    cli.table('PR:', `PR: https:/github.com/nodejs/node/${prid}`);
+    cli.table('State:', state);
+    cli.table(`Username`, username);
+    cli.table('Upstream', upstream);
+    cli.table('Branch', branch);
+  } else {
+    cli.warn('No landing session in progress');
+  }
+}
+
+module.exports = {
+  command: 'status',
+  describe: 'Return status of the current git-node land session',
+  handler: handler
+};
