0d49edaf6736b393b0597ee67d70381cd5841b40
Feng Yu <F3n67u@outlook.com>
2022-07-30 17:05:18 +0800

    fix: parse ci failure error (#640)

--- test/unit/ci_failure_parser.test.js
+++ test/unit/ci_failure_parser.test.js
@@ -18,4 +18,18 @@ describe('Jenkins', () => {
       fixtures.readJSON('jenkins', 'git-failure-2', 'expected.json');
     assert.deepStrictEqual(raw(actual), expected);
   });
+
+  // Test for issue https://github.com/nodejs/node-core-utils/issues/639
+  it('should parse test failure of test runner', async() => {
+    const text =
+      fixtures.readFile('jenkins', 'test-runner-test-failure', 'console.txt');
+    const parser = new CIFailureParser({
+      jobUrl: 'https://ci.nodejs.org/job/node-test-commit-osx-arm/nodes=osx11/6298/',
+      builtOn: 'test-nearform-macos11.0-arm64-1'
+    }, text);
+    const actual = parser.parse();
+    const expected =
+      fixtures.readJSON('jenkins', 'test-runner-test-failure', 'expected.json');
+    assert.deepStrictEqual(raw(actual), expected);
+  });
 });
