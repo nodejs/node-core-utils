85ea8361617308d0ef13de5866688902783cdb96
Matteo Collina <hello@matteocollina.com>
2024-05-02 10:49:58 +0200

    Revert "fix(ncu-ci): fix missing `await` causing all CI requests to be rejected (#799)"
    This reverts commit 7fc2f9de22532381ebbbdb0373f876ccdb859920.

--- test/unit/ci_start.test.js
+++ test/unit/ci_start.test.js
@@ -124,7 +124,7 @@ describe('Jenkins', () => {
         const cli = new TestCLI();
 
         sinon.replace(PRChecker.prototype, 'checkCommitsAfterReview',
-          sinon.fake.returns(Promise.resolve(certifySafe)));
+          sinon.fake.returns(certifySafe));
 
         const request = {
           gql: sinon.stub().returns({
